4b5733d3f0123c269c2b88bc58e7d6e256df2983
Fitri Ratna Dewi <fratnadewi@gmail.com>
2022-03-14 20:48:43 +0700

    Add alt attribute

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,8 @@
-import React from "react";
-import SignUp from "../components/SignUp";
-import SignIn from "../components/SignIn";
-import UseModal from "../components/UseModal";
-import "../App.css";
+import React from 'react';
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
+import UseModal from '../components/UseModal';
+import '../App.css';
 
 const Landing = () => {
   const { isShowing, toggle } = UseModal();
@@ -10,15 +10,15 @@ const Landing = () => {
 
   return (
     <div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-6 px-5 py-3">
-            <div className="row float-left">
-              <img src={require("../images/Logo.svg")} alt=""></img>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-6 px-5 py-3'>
+            <div className='row float-left'>
+              <img src={require('../images/Logo.svg')} alt=''></img>
             </div>
 
-            <div className="row float-left">
-              <h1 className="landing-text">
+            <div className='row float-left'>
+              <h1 className='landing-text'>
                 <i>Your</i> library anywhere
               </h1>
               <h5>
@@ -27,17 +27,17 @@ const Landing = () => {
               </h5>
             </div>
 
-            <div className="row">
+            <div className='row'>
               <button
-                className="Button-o mt-3 mr-3"
-                style={{ width: "45%" }}
+                className='Button-o mt-3 mr-3'
+                style={{ width: '45%' }}
                 onClick={toggle}
               >
                 Sign Up
               </button>
               <button
-                className="Button-g mt-3"
-                style={{ width: "45%" }}
+                className='Button-g mt-3'
+                style={{ width: '45%' }}
                 onClick={toggleSignIn}
               >
                 Sign In
@@ -47,13 +47,14 @@ const Landing = () => {
             </div>
           </div>
 
-          <div className="col-6">
+          <div className='col-6'>
             <img
-              src={require("../images/Vector1.svg")}
+              alt=''
+              src={require('../images/Vector1.svg')}
               style={{
-                maxWidth: "100%",
-                maxHeight: "100vh",
-                float: "right",
+                maxWidth: '100%',
+                maxHeight: '100vh',
+                float: 'right',
               }}
             ></img>
           </div>
